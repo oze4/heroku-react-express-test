918cbb0f5897b84a145319c99d53f9ea7298701f
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-22 01:19:08 -0600

    init

--- client/src/Pages/RouteNotFound.js
+++ client/src/Pages/RouteNotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RouteNotFound() {
     return (
         <div>
-            <h1 style="color: red">Uh Oh, can't find that :(</h1>
+            <h1 style={{color: 'red'}}>Uh Oh, can't find that :(</h1>
         </div>
     );
 }
